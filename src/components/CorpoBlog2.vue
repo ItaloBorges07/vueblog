785782898496b6190f4c5cdb3e8331532263532b
ItaloBorges07 <italo.borges@etec.sp.gov.br>
2024-06-19 00:22:01 -0300

    meu projeto

---
+++ src/components/CorpoBlog2.vue
@@ -0,0 +1,52 @@
+<script setup>
+defineProps({
+  titulo: {
+    type: String,
+    required: true
+  },
+  txt: {
+    type: String,
+    required: true
+  },
+  post: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div class="largura">
+        <div class="cab">
+            <div id="div_logo">
+                <span>
+                    <img :src="post">
+                </span>
+            </div>
+            <div id="div_titulo">
+                <span>{{ titulo }}</span>
+            </div>
+            <div id="div_txt">
+                <span>{{ txt }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#div_titulo{
+    color: red;
+    font-family: 'Times New Roman';
+}
+#div_txt{
+    font-family: 'Arial';
+}
+.cab{
+    text-align: justify;
+}
+.largura{
+    width: 100%;
+    align-items: center;
+    margin: auto;
+}
+</style>
